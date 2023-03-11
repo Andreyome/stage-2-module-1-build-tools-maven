c9ccf14fb7eccfe2050ec3842cbe74ef8ed4573a
Andreyome <andreyomem@gmail.com>
2023-03-11 11:34:43 +0200

    fix last

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,12 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        try{return Float.parseFloat(str)>0;}
+        try{
+            if(str.startsWith("01")||str.startsWith("02") || str.startsWith("03") || str.startsWith("04") || str.startsWith("05") || str.startsWith("06") || str.startsWith("07") || str.startsWith("08") || str.startsWith("09")){
+            return false;
+        }
+            return Float.parseFloat(str)>0;
+        }
         catch(Exception e){
             return false;
         }
