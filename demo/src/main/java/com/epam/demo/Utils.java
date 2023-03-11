c0b0a9c5536337c93c33957b23ba8b213fe3aa97
Andreyome <andreyomem@gmail.com>
2023-03-11 10:44:51 +0200

    commit 1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,21 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        try {
+        for (String str : args) {
+            if(str.startsWith("01")||str.startsWith("02") || str.startsWith("03") || str.startsWith("04") || str.startsWith("05") || str.startsWith("06") || str.startsWith("07") || str.startsWith("08") || str.startsWith("09")){
+                return false;
+            }
+            if (Float.parseFloat(str) <= 0) {
+                return false;
+            }
+        }
+        return true;
+        //magic happens here
+    }
+    catch (Exception e){
+        return false;
+    }
        //magic happens here
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        try{return Float.parseFloat(str)>0;}
+        catch(Exception e){
+            return false;
+        }
         //code!
     }
 }
